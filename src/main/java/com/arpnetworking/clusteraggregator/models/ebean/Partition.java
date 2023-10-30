d2a45923ba2c4f2cacd8a19a4897481d0e6c7932
Brandon Arp <brandonarp@gmail.com>
2023-10-30 11:25:02 -0700

    use jakarta imports

--- src/main/java/com/arpnetworking/clusteraggregator/models/ebean/Partition.java
+++ src/main/java/com/arpnetworking/clusteraggregator/models/ebean/Partition.java
@@ -18,20 +18,20 @@ package com.arpnetworking.clusteraggregator.models.ebean;
 import com.arpnetworking.utility.Database;
 import io.ebean.annotation.WhenCreated;
 import io.ebean.annotation.WhenModified;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
 
 import java.sql.Timestamp;
 import java.util.List;
 import javax.annotation.Nullable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.Version;
 
 /**
  * Model that holds the count of entries in a partition.
